aa4025d617c7b4a9d90587f1553b29dc710bf2a8
itest-lab <hr46.ksg@gmail.com>
2025-08-03 01:07:29 +0900

    Update fetchStatus.js

--- api/fetchStatus.js
+++ api/fetchStatus.js
@@ -145,6 +145,8 @@ const configs = {
 };
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin",  "*");
+  res.setHeader("Access-Control-Allow-Methods","GET,OPTIONS");
   // CORS preflight
   if (req.method === "OPTIONS") {
     res.setHeader("Access-Control-Allow-Origin", "*");
